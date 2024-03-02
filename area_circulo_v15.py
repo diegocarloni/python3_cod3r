f232d58132a22d6348e3e81946d1172f9c22b83d
‘Diego_Carloni’ <diego.carloni@hotmail.com>
2024-03-02 19:04:52 -0300

    validando se o parametro é numerico inteiro apenas com dois blocos de ifs

---
+++ area_circulo_v15.py
@@ -0,0 +1,28 @@
+from math import pi
+import sys #importa o módulo sys, que é um módulo que fornece acesso a algumas variáveis usadas ou mantidas pelo interpretador e a funções que interagem fortemente com o interpretador.
+import errno #importa o módulo errno, que fornece um número de erro simbólico. O módulo errno define várias constantes, como EPERM, EPIPE, etc. que representam códigos de erro que podem ser retornados por várias funções do sistema operacional.
+
+
+def ajuda():
+    print('É necessário informar o raio do círculo.')
+    print('Sintaxe: {} <raio>'.format(sys.argv[0][25:]))
+
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2: #se o tamanho da lista de argumentos for menor que 2 (ou seja, se não houver argumentos) 
+        ajuda()
+        #sys.exit(1) #termina o programa com erro 1
+        sys.exit(errno.EPERM) #termina o programa com erro EPERM, que é um código de erro que representa a operação não permitida. Mesmo que o erro seja diferente, o programa termina com erro 1
+
+    if not sys.argv[1].isnumeric(): #se o argumento 1 não for numérico inteiro
+        ajuda()
+        print('O raio deve ser um valor numérico inteiro.')
+        sys.exit(errno.EINVAL) #termina o programa com erro EINVAL, que é um código de erro que representa um argumento inválido
+
+    raio = sys.argv[1] #argumento 1 na linha de comando
+    area = circulo(raio)
+    print('Área do círculo: ', area)
